04d1bb8e79c64b23e3de40c17c92fd9b0fbe5c5f
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-04-09 22:56:36 +0300

    создал приложение, позволяющее выводить на экран и в консоль введёные логин и пароль

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react"; 
+import Info from "./components2/info2.js";
+import Form from "./components2/form2.js";
+import Login from "./components2/login2.js";
+
+class App extends React.Component {
+
+  state = {
+    login: undefined,
+    password: undefined,
+    error: undefined
+  }
+
+  gettingForm = async (e) => {
+    e.preventDefault();
+    let login = e.target.elements.login.value;
+    let password = e.target.elements.password.value;
+      console.log(login);
+      console.log(password);
+
+    if (login) {
+
+      const dataLog = await login;
+      const dataPass = await password;
+
+      this.setState({
+        login: dataLog,
+        password: dataPass,
+        error: undefined
+      });
+    } else {
+      this.setState({
+        login: undefined,
+        password: undefined,
+        error: "Заполните то, что пропустили"
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-5 info">
+                <Info />
+              </div>
+              <div className="col-sm-7 form">
+                <Form loginMethod={this.gettingForm} />
+                <Login
+                  login={this.state.login}
+                  password={this.state.password}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
