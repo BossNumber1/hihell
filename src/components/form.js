f255f4682ed03d4cc4f99ed0a259c3f29311e44d
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-04-11 20:19:29 +0300

    добавил id форме и полю с логином

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Form = props => (
-  <form onSubmit={props.loginMethod}>
-    <input type="text" name="login" placeholder="Введите ваш логин" />
+  <form onSubmit={props.loginMethod} id="messForm">
+    <input type="text" name="login" id="login" placeholder="Введите ваш логин" />
     <input type="password" name="password" placeholder="Введите ваш пароль" />
       <button>Отправить данные</button>
   </form>
