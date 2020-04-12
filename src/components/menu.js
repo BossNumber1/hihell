f21a8247d0198dc8a1d21c7b9f7758231b88385c
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-04-13 00:02:33 +0300

    создал компонент Menu

---
+++ src/components/menu.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+const Menu = () => (
+    <div>
+
+<div id="mypopup" class="popup"> 
+
+  <div class="popup-content"> 
+    
+    <div class="popup-header">
+     
+      <span class="close" class="hint" data-title="закрыть меню">&times;</span>
+    </div> 
+                
+     <div class="popup-body"> 
+                    
+        <p class="img">
+        <img src="img/ava.jpg" alt="Аватарка" />
+      </p>
+      
+      <p class="nikname"> 
+            tvoya_techa
+      </p>
+      
+      <p class="set">
+        <a href="set.html" class="hint" data-title="Настройки">
+          <img src="img/set.jpg" alt="Настройки"/>
+        </a>
+      </p>
+      
+      <p class="poisk">
+        <a href="poisk.html" class="hint" data-title="Поиск">
+          <img src="img/poisk.jpg" alt="Поиск"/>
+        </a>
+      </p>
+      
+      <p class="friends">
+        <a href="friends.html" class="hint" data-title="Друзья">
+          <img src="img/friends.jpg" alt="Друзья"/>
+        </a>
+      </p> 
+
+<a href="#">
+    <span>
+                      <p class="sms">
+                        <a href="sms.html" class="hint" data-title="Письма"
+                        ><img src="img/sms.jpg" alt="Смс"
+                        /></a>
+                      </p> 
+                    
+
+ <p class="sms">
+                        <a href="sms.html" class="hint" data-title="Письма"> 
+                        
+                        <img src="img/sms.jpg" alt="Смс"/>    
+                      
+                      </a>
+                 </p>
+
+  </span>
+</a> 
+
+
+    </div>
+    
+    <div class="popup-footer"> 
+      
+        <p class="exit">
+        <a href="" class="hint" data-title="Выйти">
+          
+          <input 
+          type="submit" 
+          style="background-image: url(img/exit.jpg); 
+            width: 45px; height: 50px;" 
+          alt="Выйти" value="" id="send-form">         
+
+                     
+        </a>
+      </p>
+
+      <p class="info">
+        <a href="info.html" class="hint" data-title="Информация">
+          <img src="img/info.jpg" alt="Информация"/>
+        </a>
+      </p>
+                
+    </div>
+  </div>
+</div>
+
+
+
+    </div>
+)
+
+export default Menu;
