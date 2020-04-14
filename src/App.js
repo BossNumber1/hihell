d4432ed9a02ce75a49f96732d35a5a1afc89f1f7
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-04-14 21:10:22 +0300

    закомментил компонент Menu

--- src/App.js
+++ src/App.js
@@ -1,56 +1,53 @@
-import io from 'socket.io-client';
-import React from "react"; 
+import io from "socket.io-client";
+import React from "react";
 // import './App.css';
-import './Main.css';
+import "./Main.css";
 // import Info from "./components/info.js";
-import Menu from "./components/menu.js";
+// import Menu from "./components/menu.js";
 import Form from "./components/form.js";
 import Login from "./components/login.js";
-class App extends React.Component {
 
+class App extends React.Component {
   state = {
     login: undefined,
     password: undefined,
-    error: undefined
-  }
+    error: undefined,
+  };
 
   gettingForm = async (e) => {
     e.preventDefault();
     let login = e.target.elements.login.value;
     let password = e.target.elements.password.value;
-      console.log(login);
-      console.log(password);
+    console.log(login);
+    console.log(password);
 
     if (login) {
-
       const dataLog = await login;
       const dataPass = await password;
 
       this.setState({
         login: dataLog,
         password: dataPass,
-        error: undefined
+        error: undefined,
       });
 
       //теперь запускаем socket.io и отправляем данные на сервер
-            
-              let socket = io.connect('localhost:3000');
-              
-                socket.emit('send mess', {login: login, mess: password});
 
-                socket.on('add mess', function (data) {
-                  console.log("проверка! передающиеся данные = ", data);
-                });
-              
-            
+      let socket = io.connect("localhost:3000");
+
+      socket.emit("send mess", { login: login, mess: password });
+
+      socket.on("add mess", function (data) {
+        console.log("проверка! передающиеся данные = ", data);
+      });
     } else {
       this.setState({
         login: undefined,
         password: undefined,
-        error: "Заполните то, что пропустили"
+        error: "Заполните то, что пропустили",
       });
     }
-  }
+  };
 
   render() {
     return (
@@ -58,10 +55,8 @@ class App extends React.Component {
         <div className="main">
           <div className="container">
             <div className="row">
-              <div className="col-sm-5 info">
-                <Menu />
-              </div>
-              <div className="col-sm-12 form">
+              <div className="col-sm-5 info">{/* <Menu /> */}</div>
+              <div className="col-sm-7 form">
                 <Form loginMethod={this.gettingForm} />
                 <Login
                   login={this.state.login}
