aa4535e289c9961497ecca412fab8f1821dc0527
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-04-16 11:42:06 +0300

    создан компонент Меню, заменяющий старый на то, что нужно

---
+++ src/components/menuDev.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MenuDev = () => (
+    <div>
+        <p class="avaExe"></p>
+
+        <p class="nikname">
+            tvoya_techa
+        </p>
+
+        <p class="set"></p>
+        <p class="poisk"></p>
+        <p class="friends"></p>
+        <p class="sms"></p>
+        <p class="inform"></p>
+        <p class="exit"></p>
+    </div>
+)
+
+export default MenuDev;
