b618c0b258002759d5c4e33f200f77567729573d
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-04-18 17:37:03 +0300

    пополнил стек состояния новыми данными, сделал возможность вывода сообщений пользователю в определённое место на сайт

--- src/App.js
+++ src/App.js
@@ -14,35 +14,131 @@ class App extends React.Component {
   state = {
     login: undefined,
     password: undefined,
+    loginReg: undefined,
+    passwordReg: undefined,
     error: undefined,
+    errorReg: undefined
   };
 
   gettingForm = async (e) => {
     e.preventDefault();
     let login = e.target.elements.login.value;
     let password = e.target.elements.password.value;
+
+    let loginReg = e.target.elements.loginReg.value;
+    let passwordReg = e.target.elements.passwordReg.value;
+    let rePasswordReg = e.target.elements.rePasswordReg.value;
     
+    let socket = io.connect("localhost:3000");
+
+    //алгоритм для авторизации
+
     if (login) {
       const dataLog = await login;
       const dataPass = await password;
 
-      this.setState({
-        login: dataLog,
-        password: dataPass,
-        error: undefined,
-      });
+          //проверяем полученные данные
+
+            let fail = false; 
+                   
+            if (dataLog === "" || dataLog === " ") {
+              fail = "Вы не ввели логин";
+            } else if (dataPass === "") {
+              fail = "Вы не ввели пароль";
+            }
+        
+            if (fail) {
+              this.setState({
+                error: fail
+              });
+            } else {
+              this.setState({
+                loginReg: undefined,
+                passwordReg: undefined,
+                login: dataLog,
+                password: dataPass,
+                error: undefined
+              });
+            }
+
+            if (fail === false) {
+              socket.emit("send mess", { login: login, mess: password });
+
+              socket.on('add mess', function(data) {
+                let all_messages = document.getElementById("all_messages");
+                all_messages.append(data.mess);
+                // all_messages.innerHTML.replace(/<a.*?undefined<\/a>,?/,'');
+                all_messages.innerHTML = all_messages.innerHTML.replace(/<a.*?undefined<\/a>,?/,'');
+              });
+            }
+      
+          } else {
+            this.setState({
+              login: undefined,
+              password: undefined,
+              loginReg: undefined,
+              passwordReg: undefined,
+              error: "Введите данные"
+            });
+          }
+    
+    //алгоритм для регистрации
+
+    if (loginReg) {
+      const dataLogReg = await loginReg;
+      const dataPassReg = await passwordReg;
+      const dataRePassReg = await rePasswordReg;
+
+      //проверяем полученные данные
+
+        let fail = false; 
+                   
+        if (dataLogReg === "" || dataLogReg === " ") {
+          fail = "Вы не ввели логин";
+        } else if (dataPassReg === "") {
+          fail = "Вы не ввели пароль";
+        } else if (dataPassReg !== dataRePassReg) {
+          fail = "Пароли не совпадают";
+        }
+    
+        if (fail) {
+          this.setState({
+            error: fail
+          });
+        } else {
+          this.setState({
+            login: undefined,
+            password: undefined,
+            loginReg: dataLogReg,
+            passwordReg: dataPassReg,
+            error: undefined
+          });
+        }
 
       //теперь запускаем socket.io и отправляем данные на сервер
 
-      let socket = io.connect("localhost:3000");
+      // let socket = io.connect("localhost:3000");
+      
+      if (fail === false) {
+        socket.emit("send mess", { loginReg: loginReg, messReg: passwordReg });
 
-      socket.emit("send mess", { login: login, mess: password });
+        socket.on('add mess', function(data) {
+          let all_messages = document.getElementById("all_messages");
+          all_messages.append(data.messRegi);
+         
+          all_messages.innerHTML = all_messages.innerHTML.replace(/<a.*?undefined<\/a>,?/,'');
+          
+          // all_messages.innerHTML.replace(/<a.*?undefined<\/a>,?/,'');
+
+        });
+      }
 
     } else {
+
       this.setState({
-        login: undefined,
-        password: undefined,
-        error: "Заполните то, что пропустили",
+        loginReg: undefined,
+        passwordReg: undefined,
+        errorReg: "Введите данные"
       });
     }
   };
@@ -55,6 +151,9 @@ class App extends React.Component {
             <div className="row">
               {/* <div> */}
               <div className="col-sm-5 info">
+                  <div id="all_messages">
+
+                  </div>
                 {/* <Menu /> */}
                 {/* <MenuDev /> */}
                 </div>
@@ -63,6 +162,8 @@ class App extends React.Component {
                 <Login
                   login={this.state.login}
                   password={this.state.password}
+                  loginReg={this.state.loginReg}
+                  passwordReg={this.state.passwordReg}
                   error={this.state.error}
                 />
               </div>
