e1f1a5196b55549d5d5992bcad3e397053696461
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-05-03 09:04:31 +0300

    создал компонент СмсТет-а-тет

---
+++ src/components/smsTAT.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SmsTAT = () => (
+    <div>
+        <input type="text" placeholder="Введите своё сообщение" id="smsTAT" />
+        <p id="smsWTAT"></p>
+        <p id="nik">Крис Асмус</p>
+        <p id="onlineTAT"></p>
+        <p id="avaUsersTAT"></p>
+        <p id="friAdd"></p>
+            <div id="smsI"></div>
+            <div id="smsI" style={{marginTop: -1470 + 'px', marginLeft: 150 + 'px'}}></div>
+        <p id="diaI">Привет</p>
+            <p id="diaI" style={{marginTop: -1405 + 'px', marginLeft: 290 + 'px'}}>Хай</p>
+        <p id="diaItrans">Hello</p>
+            <p id="timeSms">19:22</p>
+            <p id="timeSms" style={{marginTop: -1405 + 'px', marginLeft: 350 + 'px'}}>19:21</p>
+    </div>
+)
+
+export default SmsTAT;
