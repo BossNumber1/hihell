b39ce68a01682e38298e7f7dc4dfc096f5e112cc
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-05-03 09:56:29 +0300

    добавил элементы сообщений и дату

--- src/components/smsTAT.js
+++ src/components/smsTAT.js
@@ -8,13 +8,19 @@ const SmsTAT = () => (
         <p id="onlineTAT"></p>
         <p id="avaUsersTAT"></p>
         <p id="friAdd"></p>
-            <div id="smsI"></div>
+        <p id="dataSms">29.01.20</p>
+        <div id="smsI"></div>
             <div id="smsI" style={{marginTop: -1470 + 'px', marginLeft: 150 + 'px'}}></div>
-        <p id="diaI">Привет</p>
-            <p id="diaI" style={{marginTop: -1405 + 'px', marginLeft: 290 + 'px'}}>Хай</p>
-        <p id="diaItrans">Hello</p>
-            <p id="timeSms">19:22</p>
+                <div id="smsI" style={{marginTop: -1560 + 'px', marginLeft: 20 + 'px'}}></div>
+        <p id="diaI">Как дела?</p>
+            <p id="diaI" style={{marginTop: -1455 + 'px', marginLeft: 230 + 'px'}}>Хай</p>
+                <p id="diaI" style={{marginTop: -1547 + 'px', marginLeft: 85 + 'px'}}>Привет</p>
+        <p id="diaItrans">How are you?</p>
+            <p id="diaItrans" style={{marginTop: -1430 + 'px', marginLeft: 235 + 'px'}}>Hi</p>
+                <p id="diaItrans" style={{marginTop: -1520 + 'px', marginLeft: 95 + 'px'}}>Hello</p>
+        <p id="timeSms">19:22</p>
             <p id="timeSms" style={{marginTop: -1405 + 'px', marginLeft: 350 + 'px'}}>19:21</p>
+                <p id="timeSms" style={{marginTop: -1495 + 'px', marginLeft: 220 + 'px'}}>19:20</p>
     </div>
 )
 
